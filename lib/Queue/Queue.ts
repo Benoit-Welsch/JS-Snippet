26ee3cb9797219059b22af4761d88e3d1972d411
Benoit Welsch <benoitwelsch@protonmail.com>
2022-12-23 21:50:59 +0100

    Feat : Queue (js + ts)

---
+++ lib/Queue/Queue.ts
@@ -0,0 +1,32 @@
+interface Job {
+  run: () => Promise<any>;
+  running: boolean;
+}
+
+export default class Queue extends Array<Job> {
+  running = 0;
+  maxjob = 1;
+
+  constructor(maxjob = 1, jobs = []) {
+    super(0);
+    this.maxjob = maxjob - 1;
+    jobs.forEach((j) => this.push(j));
+  }
+
+  async exec() {
+    if (this.running > this.maxjob || this.length <= 0) return;
+    const job = this.shift();
+    if (!job || !job.run || job.running) return;
+    this.running += 1;
+    if (this.running < this.maxjob) this.exec();
+    return job.run().then(() => {
+      this.running -= 1;
+      this.exec();
+    });
+  }
+
+  add(f: () => Promise<any>) {
+    this.push({ run: f, running: false });
+    this.exec();
+  }
+}
