7c35b28eea2d815c5428e39306d6ad556719d3fc
Benoit-Welsch <benoitwelsch@protonmail.com>
2023-11-27 18:46:25 +0000

    Feat : mini repo

--- build.mjs
+++ build.mjs
@@ -1,4 +1,7 @@
 import dts from "bun-plugin-dts";
+import { Scanner } from "./src";
+import path from 'path';
+import fs from 'fs/promises';
 
 await Bun.build({
   entrypoints: ["./src/index.ts"],
@@ -7,3 +10,43 @@ await Bun.build({
   minify: true,
   plugins: [dts()],
 });
+
+Scanner
+  .readFolder("./src")
+  .folders
+  .map(async (f) => {
+    const name = path.basename(f.path)
+    const entrypoints = [path.join("./src", name, "index.ts")]
+    const outdir = path.join("./dist_standalone", name)
+
+    await fs.mkdir(outdir, { recursive: true })
+
+    await Bun.build({
+      entrypoints,
+      outdir,
+      target: "bun",
+      minify: true,
+      plugins: [dts()],
+    });
+
+    const pkg = JSON.parse(await fs.readFile(path.join(f.path, "package.json"), { encoding: "utf-8" }))
+
+    await fs.writeFile(path.join(outdir, "package.json"), JSON.stringify({
+      ...pkg,
+      name: `@lv00/toolkit.${name.toLowerCase()}`,
+      description: `reduce toolkit including only ${name}`,
+      module: "./index.js",
+      type: "module",
+      main: "./index.js",
+      exports: {
+        '.': {
+          types: "./index.d.ts",
+          default: "./index.js"
+        }
+      },
+    })
+    )
+  }
+  )
+
+
