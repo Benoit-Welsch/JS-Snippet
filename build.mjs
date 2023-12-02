b6e138ada4aa4fa2a3ffdbaaed815be966bb36f3
Benoit-Welsch <benoitwelsch@protonmail.com>
2023-12-02 21:10:33 +0000

    Reduce toolkit disable for the moment

--- build.mjs
+++ build.mjs
@@ -11,46 +11,46 @@ await Bun.build({
   plugins: [dts()],
 });
 
-Scanner.readFolder("./src").folders.map(async (f) => {
-  const name = path.basename(f.path);
-  const entrypoints = [path.join("./src", name, "index.ts")];
-  const outDir = path.join("./dist_standalone", name);
+// Scanner.readFolder("./src").folders.map(async (f) => {
+//   const name = path.basename(f.path);
+//   const entrypoints = [path.join("./src", name, "index.ts")];
+//   const outDir = path.join("./dist_standalone", name);
 
-  await fs.mkdir(outDir, { recursive: true });
+//   await fs.mkdir(outDir, { recursive: true });
 
-  await Bun.build({
-    entrypoints,
-    outDir,
-    target: "bun",
-    minify: true,
-    plugins: [dts()],
-  });
+//   await Bun.build({
+//     entrypoints,
+//     outDir,
+//     target: "bun",
+//     minify: true,
+//     plugins: [dts()],
+//   });
 
-  await fs.copyFile(
-    path.join("./src", name, "Readme.md"),
-    path.join(outDir, "Readme.md"),
-  );
+//   await fs.copyFile(
+//     path.join("./src", name, "Readme.md"),
+//     path.join(outDir, "Readme.md"),
+//   );
 
-  const pkg = JSON.parse(
-    await fs.readFile("package.json", { encoding: "utf-8" }),
-  );
+//   const pkg = JSON.parse(
+//     await fs.readFile("package.json", { encoding: "utf-8" }),
+//   );
 
-  await fs.writeFile(
-    path.join(outDir, "package.json"),
-    JSON.stringify({
-      ...pkg,
-      scripts: {},
-      name: `@lv00/toolkit.${name.toLowerCase()}`,
-      description: `reduce toolkit including only ${name}`,
-      module: "./index.js",
-      type: "module",
-      main: "./index.js",
-      exports: {
-        ".": {
-          types: "./index.d.ts",
-          default: "./index.js",
-        },
-      },
-    }),
-  );
-});
+//   await fs.writeFile(
+//     path.join(outDir, "package.json"),
+//     JSON.stringify({
+//       ...pkg,
+//       scripts: {},
+//       name: `@lv00/toolkit.${name.toLowerCase()}`,
+//       description: `reduce toolkit including only ${name}`,
+//       module: "./index.js",
+//       type: "module",
+//       main: "./index.js",
+//       exports: {
+//         ".": {
+//           types: "./index.d.ts",
+//           default: "./index.js",
+//         },
+//       },
+//     }),
+//   );
+// });
