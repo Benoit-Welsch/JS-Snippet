56b680eac6790b99857d596b98fbc52c48ff48b0
Benoit-Welsch <benoitwelsch@protonmail.com>
2024-09-11 10:35:52 +0000

    Doc : CSV example

--- src/Csv/index.ts
+++ src/Csv/index.ts
@@ -1,24 +1,60 @@
 export default class CSV extends Array<Array<string | number>> {
   maxCol = 0;
 
+  /**
+   * Create a CSV object
+   * @param header - The header of the CSV
+   * @example
+   * const csv = new CSV({ header: ['First Name', 'Last Name'] });
+   */
   constructor({ header }: { header: string[] }) {
     super(new Array<string>(1));
     if (header) this.setHeader(header);
   }
 
+  /**
+    * Set the header of the CSV
+   * @param head - The header of the CSV
+   */
   setHeader(head: string[]) {
     this[0] = head;
     this.maxCol = head.length;
   }
 
+  /**
+   * Get the header of the CSV
+   * @returns The header of the CSV
+   * @example
+   * const csv = new CSV({ header: ['First Name', 'Last Name'] });
+   * csv.getHeaders(); // Output: ['First Name', 'Last Name']
+    */
   getHeaders() {
     return this[0];
   }
 
+  /**
+   * Get the lines of the CSV
+   * @returns The lines of the CSV
+   * @example
+   * const csv = new CSV({ header: ['First Name', 'Last Name'] });
+   * csv.addLine(['John', 'Doe']);
+   * csv.addLine(['Jane', 'Doe']);
+   * csv.getLines(); // Output: [['John', 'Doe'], ['Jane', 'Doe']]
+   */
   getLines() {
     return this.slice(1);
   }
 
+  /**
+   * Convert the CSV to an array of objects
+   * @param UpperCaseAfterSapce - If true, the first letter after a space will be upper case
+   * @returns An array of objects
+   * @example
+   * const csv = new CSV({ header: ['First Name', 'Last Name'] });
+   * csv.addLine(['John', 'Doe']);
+   * csv.addLine(['Jane', 'Doe']);
+   * csv.toObject(); // Output: [{ firstName: 'John', lastName: 'Doe' }, { firstName: 'Jane', lastName: 'Doe' }]
+   */
   toObject(UpperCaseAfterSapce = true) {
     const headers = this.getHeaders();
     return this.getLines().map((line) => {
@@ -34,6 +70,19 @@ export default class CSV extends Array<Array<string | number>> {
     });
   }
 
+  /**
+   * Work like Array.forEach
+   * @param header - If true, the header will be included (default: false)
+   * @param callbackfn - A function that accepts up to three arguments. forEach calls the callbackfn function one time for each element in the array.
+   * @param thisArg - An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+   * @example
+   * const csv = new CSV({ header: ['First Name', 'Last Name'] });
+   * csv.addLine(['John', 'Doe']);
+   * csv.addLine(['Jane', 'Doe']);
+   * csv.each((line, index) => {
+   *  console.log(line); // Output: ['John', 'Doe'] and ['Jane', 'Doe']
+   * });
+    */
   each(
     header = false,
     callbackfn: (
@@ -46,6 +95,16 @@ export default class CSV extends Array<Array<string | number>> {
     (header ? this : this.slice(1)).forEach(callbackfn, thisArg);
   }
 
+  /**
+   * 
+   * @param line - The line to add
+   * @param smallerConsideredAsError - If true, the line length must be equal to the header length (default: true)
+   * @example
+   * const csv = new CSV({ header: ['First Name', 'Last Name'] });
+   * csv.addLine(['John', 'Doe']);
+   * csv.addLine(['Jane', 'Doe'], false); // No error
+   * csv.addLine(['Jane', 'Doe', 'Doe']); // Error
+   */
   addLine(line: Array<string | number>, smallerConsideredAsError = true) {
     if (line.length !== this.maxCol && smallerConsideredAsError) {
       throw new Error(
@@ -56,6 +115,18 @@ export default class CSV extends Array<Array<string | number>> {
     this.push(line);
   }
 
+  /**
+   * Add a string or number to the CSV sequentially
+   * @param s - The string or number to add
+   * @returns The added string or number
+   * @example
+   * const csv = new CSV({ header: ['First Name', 'Last Name'] });
+   * csv.addSequentially('John');
+   * csv.addSequentially('Doe');
+   * csv.addSequentially('Jane');
+   * csv.addSequentially('Doe');
+   * csv.toObject(); // Output: [{ firstName: 'John', lastName: 'Doe' }, { firstName: 'Jane', lastName: 'Doe' }]
+   */
   addSequentially(s: string | number): string | number {
     const lastLine = this[this.length - 1];
     if (lastLine.length >= this.maxCol || this.length === 1) {
@@ -64,6 +135,18 @@ export default class CSV extends Array<Array<string | number>> {
     }
     return lastLine.push(s);
   }
+
+  /**
+   * Convert the CSV to a string with break lines and separators
+   * @param separator
+   * @param header 
+   * @returns A string
+   * @example
+   * const csv = new CSV({ header: ['First Name', 'Last Name'] });
+   * csv.addLine(['John', 'Doe']);
+   * csv.addLine(['Jane', 'Doe']);
+   * csv.toString(); // Output: "First Name";"Last Name"\r\n"John";"Doe"\r\n"Jane";"Doe"
+   */
 
   toString(separator = ';', header = true): string {
     const copy = this.slice(header ? 0 : 1);
@@ -76,6 +159,16 @@ export default class CSV extends Array<Array<string | number>> {
       .join('\r\n');
   }
 
+  /**
+   * Convert the CSV to a string with break lines and separators
+   * @param separator
+   * @param rules - An array of rules to apply to the CSV
+   * @returns A string
+   * @example
+   * const csv = new CSV({ header: ['First Name', 'Last Name'] });
+   * csv.addLine(['John', 'Doe']);
+   * csv.toStringEncoded(';', [{ from: ' ', to: '_' }]); // Output: "First_Name";"Last_Name"\r\n"John";"Doe"
+    */
   toStringEncoded(
     separator = ';',
     rules: { from: RegExp | string; to: string }[],
@@ -96,10 +189,31 @@ export default class CSV extends Array<Array<string | number>> {
       .join('\r\n'); // Add break line
   }
 
+  /**
+   * Clear the CSV (remove all lines) but keep the header
+   * @example
+   * const csv = new CSV({ header: ['First Name', 'Last Name'] });
+   * csv.addLine(['John', 'Doe']);
+   * csv.clear();
+   * csv.toObject(); // Output: []
+   * csv.getHeaders(); // Output: ['First Name', 'Last Name']
+   */
   clear() {
     this.splice(1);
   }
 
+  /**
+   * Read a CSV string and return a CSV object
+   * @param csv - The CSV string
+   * @param separator - The separator of the CSV (default: ';')
+   * @param headers - Custom headers (default: first line)
+   * @param smallerConsideredAsError - If true, the line length must be equal to the header length (default: true)
+   * @returns A CSV object
+   * @example
+   * const csv = CSV.readString('First Name;Last Name\r\nJohn;Doe\r\nJane;Doe');
+   * csv.toObject(); // Output: [{ firstName: 'John', lastName: 'Doe' }, { firstName: 'Jane', lastName: 'Doe' }]
+   * csv.getHeaders(); // Output: ['First Name', 'Last Name']
+   */
   static readString(
     csv: string,
     separator = ';',
