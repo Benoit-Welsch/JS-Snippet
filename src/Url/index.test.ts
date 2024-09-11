40d19ad4c5a52a0890012ed2bf8ba18dcf9913fc
Benoit-Welsch <benoitwelsch@protonmail.com>
2024-09-11 10:16:11 +0000

    Feat : urlQueryBuilder support number and boolean

--- src/Url/index.test.ts
+++ src/Url/index.test.ts
@@ -3,11 +3,14 @@ import { buildUrlWithQuery } from '.';
 
 it('Should get a url with query', () => {
   const query = {
-    p: '2',
+    p: 2,
     brand: ['sony', 'microsoft', 'nintendo'],
+    s: 'game',
+    private: false,
   };
   const url = buildUrlWithQuery('https://lv0.eu/', query);
   expect(url.toString()).toBe(
-    'https://lv0.eu/?p=2&brand=sony&brand=microsoft&brand=nintendo',
+    'https://lv0.eu/?p=2&brand=sony&brand=microsoft&brand=nintendo&s=game&private=false',
   );
 });
+
--- src/Url/index.ts
+++ src/Url/index.ts
@@ -1,16 +1,26 @@
 export const buildUrlWithQuery = (
   inUrl: string,
-  query: Record<string, string | string[] | undefined>,
+  query: Record<string, string | string[] | number | number[] | boolean | undefined>,
 ) => {
   const url = new URL(inUrl);
   Object.keys(query).forEach((key) => {
     const currentQuery = query[key];
     if (Array.isArray(currentQuery)) {
-      currentQuery.forEach((qArr) => url.searchParams.append(key, qArr));
+      currentQuery.forEach((qArr) => url.searchParams.append(key, qArr.toString()));
     } else {
-      if (currentQuery)
-        url.searchParams.set(key, currentQuery);
+      if (currentQuery !== undefined && currentQuery !== null)
+        url.searchParams.set(key, currentQuery.toString());
     }
   });
   return url;
 };
+
+class Uri {
+
+  constructor(private url: string, private query: Record<string, string[]>) { }
+
+  fetchJson = async (query: Record<string, string | string[] | undefined>) => {
+    const response = await fetch(buildUrlWithQuery(this.url, query).toString());
+    return response.json();
+  }
+}
