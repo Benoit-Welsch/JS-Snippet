e9449285b257e8f68fc7f897e8aa65e875ea5bb0
Benoit-Welsch <benoitwelsch@protonmail.com>
2024-10-27 19:13:21 +0000

    Rewriting genDoc

--- genDoc.ts
+++ genDoc.ts
@@ -1,25 +1,13 @@
 import { writeFileSync } from 'fs';
 import { Scanner } from './src';
 
-const root = Scanner.readFolder('./src');
+console.log('Generating documentation...');
 
-const getToc = (names: (string | undefined)[]) => {
-  return names
-    .map((name) => {
-      if (!name) return '';
-      return `- [${name}](#${name.toLowerCase().replace(/ /g, '-')})`;
-    })
-    .join('\n');
-};
+const root = Scanner.readFolder('./src');
 
 const allDoc = root
   .flat()
   .filter((file) => file.name.endsWith('.md'))
-  .flat();
-
-const base = allDoc.find((file) => file.name === 'Base.md')?.read() || '';
-const docs = allDoc
-  .filter((file) => file.name !== 'Base.md')
   .sort((a, b) => {
     const name = a.path.split('/')[1];
     const name2 = b.path.split('/')[1];
@@ -28,12 +16,23 @@ const docs = allDoc
     return 0;
   });
 
-const toc = getToc(docs.map(({ path }) => path.split('/')[1]));
-const docsString = docs.map((f) => f.read()).join('\n');
+let out = allDoc.find((file) => file.name === 'Base.md')?.read() || '';
 
-let out = base;
+const docs = allDoc.filter((file) => file.name !== 'Base.md');
+
+const toc = docs
+  .map((d) => d.parent.name)
+  .map((name) => `- [${name}](#${name.toLowerCase().replace(/ /g, '-')})`)
+  .join('\n');
+
+const docsString = docs.map((f) => f.read()).join('\n');
 
 out = out.replaceAll('{% toc %}', toc);
 out = out.replaceAll('{% docs %}', docsString);
 
-writeFileSync('./Readme.md', out);
+try {
+  writeFileSync('./Readme.md', out);
+  console.log('Documentation generated');
+} catch (e) {
+  console.error(e);
+}
